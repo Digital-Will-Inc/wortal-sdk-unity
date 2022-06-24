9ff7461a44996fc27fbd0250c47f34be9bac4a8d
Tim Dillon <tim@itsdits.com>
2022-06-24 10:31:39 +0900

    feat: add ad provider interface

---
+++ Assets/DigitalWill/Wortal/IAdProvider.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace DigitalWill.H5Portal
+{
+    /// <summary>
+    /// Interface for ad providers. Used for implementation ad calls for different platforms.
+    /// </summary>
+    public interface IAdProvider
+    {
+        /// <summary>
+        /// Subscribe to be notified when a call is made to retrieve an ad. This is useful for starting a timer to
+        /// check for error conditions and prevent the game from waiting indefinitely.
+        /// </summary>
+        event Action AdCalled;
+        /// <summary>
+        /// Was an ad successfully returned or not. This gets set in the beforeAd callback and checked in Wortal.cs
+        /// on a timer to ensure we don't get stuck in an infinite loop waiting for an due to an error.
+        /// </summary>
+        bool IsAdAvailable { get; }
+
+        /// <summary>
+        /// Shows an interstitial ad.
+        /// </summary>
+        /// <param name="type">Type of ad placement.</param>
+        /// <param name="name">Name of the ad placement.</param>
+        void ShowInterstitialAd(Placement type, string name);
+        /// <summary>
+        /// Requests a rewarded ad. Should be called before <see cref="ShowRewardedAd"/>.
+        /// </summary>
+        /// <param name="name">Name of the ad to be shown.</param>
+        void RequestRewardedAd(string name);
+        /// <summary>
+        /// Shows a rewarded ad.
+        /// </summary>
+        void ShowRewardedAd();
+    }
+}
