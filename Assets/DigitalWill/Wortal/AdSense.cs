78817eaa251affc6bd9c86f3e81d14a96583d837
Tim Dillon <tim@itsdits.com>
2022-06-24 11:48:43 +0900

    feat: add adsense plugin

--- Assets/DigitalWill/Wortal/AdSense.cs
+++ Assets/DigitalWill/Wortal/AdSense.cs
@@ -1,196 +1,176 @@
-using System;
+using System.Runtime.InteropServices;
 using UnityEngine;
-
-#if UNITY_WEBGL && !UNITY_EDITOR
-using AFG.Api;
 using AOT;
-#endif
 
-namespace DigitalWill.Wortal
+namespace DigitalWill.H5Portal
 {
     /// <summary>
-    /// Handles calls to the AFG plugin to serve ads and delegates for the callback functions.
+    /// Handles ads provided by AdSense.
     /// </summary>
-    public class AdSense
+    internal class AdSense : IAdProvider
     {
-#pragma warning disable 67
-        /// <summary>
-        /// Subscribe to be notified when a call is made to retrieve an ad. This is useful for starting a timer to
-        /// check for error conditions and prevent the game from waiting indefinitely.
-        /// </summary>
-        public static event Action AdCalled;
-#pragma warning restore 67
+        private static bool _isAdAvailable;
+        public bool IsAdAvailable => _isAdAvailable;
 
-        /// <summary>
-        /// Was an ad successfully returned or not. This gets set in the beforeAd callback and checked in Wortal.cs
-        /// on a timer to ensure we don't get stuck in an infinite loop waiting for an due to an error.
-        /// </summary>
-        public static bool IsAdAvailable { get; private set; }
+        private delegate void BeforeAdDelegate();
+        private delegate void AfterAdDelegate();
+        private delegate void AdBreakDoneDelegate();
+        private delegate void BeforeRewardDelegate();
+        private delegate void AdDismissedDelegate();
+        private delegate void AdViewedDelegate();
+        private delegate void NoShowDelegate();
 
-        /// <summary>
-        /// Shows an interstitial ad.
-        /// </summary>
-        /// <param name="type">Type of ad placement. Corresponds with Google's AdSense placement types.</param>
-        /// <param name="name">Name of the ad placement.</param>
-        public static void ShowInterstitialAd(Placement type, string name)
+        public void ShowInterstitialAd(Placement type, string name)
         {
-            IsAdAvailable = false;
-            AdCalled?.Invoke();
-
-#if UNITY_WEBGL && !UNITY_EDITOR
-            string afgType;
+            string typeArg;
             switch (type)
             {
                 case Placement.Start:
-                    afgType = InterstitialAdType.Start;
+                    typeArg = "start";
                     break;
                 case Placement.Next:
-                    afgType = InterstitialAdType.Next;
+                    typeArg = "next";
                     break;
                 case Placement.Pause:
-                    afgType = InterstitialAdType.Pause;
+                    typeArg = "pause";
                     break;
                 case Placement.Browse:
-                    afgType = InterstitialAdType.Browse;
+                    typeArg = "browse";
                     break;
                 default:
-                    Debug.LogError("AdSense.ShowInterstitialAd was called with an invalid type.");
+                    Debug.LogError("[Wortal] AdSense.ShowInterstitialAd was called with an invalid type.");
                     return;
             }
 
-            AdBreak.ShowInterstitialAd(
-                afgType,
+            ShowInterstitialAdAdSense(
+                typeArg,
                 name,
                 BeforeAdCallback,
                 AfterAdCallback,
-                AdBreakDoneCallback);
-#endif
+                AdBreakDoneCallback,
+                NoShowCallback);
         }
 
-        /// <summary>
-        /// Requests a rewarded ad.
-        /// </summary>
-        /// <param name="name">Name of the ad to be shown.</param>
-        public static void RequestRewardedAd(string name)
+        public void RequestRewardedAd(string name)
         {
-            IsAdAvailable = false;
-            AdCalled?.Invoke();
-
-#if UNITY_WEBGL && !UNITY_EDITOR
-            AdBreak.RequestRewardedAd(
+            RequestRewardedAdAdSense(
                 name,
                 BeforeAdCallback,
                 AfterAdCallback,
                 AdBreakDoneCallback,
                 BeforeRewardCallback,
                 AdDismissedCallback,
-                AdViewedCallback);
-#endif
+                AdViewedCallback,
+                NoShowCallback);
         }
 
-        /// <summary>
-        /// Shows a rewarded ad. Should be used in the BeforeRewardCallback to be called after RequestRewardedAd has
-        /// returned an ad to show, but we provide an API for it anyways for situations where we might want to do
-        /// something in the game before beginning the ad.
-        /// </summary>
-        public static void ShowRewardedAd()
+        public void ShowRewardedAd()
         {
-#if UNITY_WEBGL && !UNITY_EDITOR
             // We don't invoke AdCalled here because there is no BeforeAdCallback attached to this method.
-            AdBreak.ShowRewardedAd();
-#endif
+            ShowRewardedAdAdSense();
         }
 
-#if UNITY_WEBGL && !UNITY_EDITOR
-        [MonoPInvokeCallback(typeof(AdBreak.BeforeAdDelegate))]
+        [DllImport("__Internal")]
+        private static extern void TriggerBeforeAdAdSense();
+
+        [DllImport("__Internal")]
+        private static extern void TriggerAfterAdAdSense();
+
+        [DllImport("__Internal")]
+        private static extern void TriggerAdBreakDoneAdSense();
+
+        [DllImport("__Internal")]
+        private static extern void TriggerBeforeRewardAdSense();
+
+        [DllImport("__Internal")]
+        private static extern void TriggerAdDismissedAdSense();
+
+        [DllImport("__Internal")]
+        private static extern void TriggerAdViewedAdSense();
+
+        [DllImport("__Internal")]
+        private static extern void TriggerNoShowAdSense();
+
+        [DllImport("__Internal")]
+        private static extern void ShowRewardedAdAdSense();
+
+        [DllImport("__Internal")]
+        private static extern void ShowInterstitialAdAdSense(string type, string name,
+                                                             BeforeAdDelegate beforeAdCallback,
+                                                             AfterAdDelegate afterAdCallback,
+                                                             AdBreakDoneDelegate adBreakDoneDelegate,
+                                                             NoShowDelegate noShowDelegate);
+
+        [DllImport("__Internal")]
+        private static extern void RequestRewardedAdAdSense(string name, BeforeAdDelegate beforeAdCallback,
+                                                            AfterAdDelegate afterAdCallback,
+                                                            AdBreakDoneDelegate adBreakDoneDelegate,
+                                                            BeforeRewardDelegate beforeRewardDelegate,
+                                                            AdDismissedDelegate adDismissedDelegate,
+                                                            AdViewedDelegate adViewedDelegate,
+                                                            NoShowDelegate noShowDelegate);
+
+        [MonoPInvokeCallback(typeof(BeforeAdDelegate))]
         private static void BeforeAdCallback()
         {
             // Called only if the ad is available and before the ad is displayed.
             // You should pause or stop the game flow here to ensure anything
             // doesn't change while the ad is being displayed.
             // We set this flag to let Wortal.cs know that we reached the BeforeAdCallback so we have an ad returned.
-            IsAdAvailable = true;
-            Debug.Log("Before Ad");
-
-
+            _isAdAvailable = true;
+            Debug.Log("[Wortal] BeforeAdCallback");
         }
 
-        [MonoPInvokeCallback(typeof(AdBreak.AfterAdDelegate))]
+        [MonoPInvokeCallback(typeof(AfterAdDelegate))]
         private static void AfterAdCallback()
         {
             // Called only if the ad is displayed after the ad is finished (for any reason).
-            Debug.Log("After Ad");
-
-
+            Debug.Log("[Wortal] AfterAdCallback");
         }
 
-        [MonoPInvokeCallback(typeof(AdBreak.AdBreakDoneDelegate))]
+        [MonoPInvokeCallback(typeof(AdBreakDoneDelegate))]
         private static void AdBreakDoneCallback()
         {
             // Called after the AdBreak finished, regardless if an ad was
             // displayed or not. You can resume or restart your game inside
             // this callback.
             // We make sure to set this here too in case we don't get an ad, but still receive a response from AdSense.
-            IsAdAvailable = true;
-            Debug.Log("AdBreak Done");
-
-
+            _isAdAvailable = true;
+            Debug.Log("[Wortal] AdBreakDoneCallback");
         }
 
-        [MonoPInvokeCallback(typeof(AdBreak.BeforeRewardDelegate))]
+        [MonoPInvokeCallback(typeof(BeforeRewardDelegate))]
         private static void BeforeRewardCallback()
         {
             // Called if a rewarded ad is available. You should show a dialog/pop-up
             // to ask if the user wants to see a rewarded ad or not. If the user agrees
             // to it you can show it with calling AdBreak.ShowRewardedAd();
-            Debug.Log("Before Reward Ad");
-
-
+            Debug.Log("[Wortal] BeforeRewardCallback");
         }
 
-        [MonoPInvokeCallback(typeof(AdBreak.AdDismissedDelegate))]
+        [MonoPInvokeCallback(typeof(AdDismissedDelegate))]
         private static void AdDismissedCallback()
         {
             // Called only for rewarded ads when the player dismisses the ad.
             // It is only called if the player dismisses the ad before it completes.
             // In this case the reward should not be granted.
-            Debug.Log("Ad Dismissed");
-
-
+            Debug.Log("[Wortal] AdDismissedCallback");
         }
 
-        [MonoPInvokeCallback(typeof(AdBreak.AdViewedDelegate))]
+        [MonoPInvokeCallback(typeof(AdViewedDelegate))]
         private static void AdViewedCallback()
         {
             // Called only for rewarded ads when the player completes the ad
             // and should be granted the reward.
-            Debug.Log("Ad Viewed");
-
-
+            Debug.Log("[Wortal] AdViewedCallback");
         }
-#endif
-    }
 
-    /// <summary>
-    /// Ad placement type defined by Google AdSense. https://developers.google.com/ad-placement/docs/placement-types
-    /// </summary>
-    public enum Placement
-    {
-        /// <summary>
-        /// Used when ad is shown before next level, restart, revive, etc.
-        /// </summary>
-        Next,
-        /// <summary>
-        /// Used only at the very beginning when the game is first loading. For preroll ads.
-        /// </summary>
-        Start,
-        /// <summary>
-        /// Used when an ad is shown after the player pauses the game.
-        /// </summary>
-        Pause,
-        /// <summary>
-        /// Used when an ad is shown outside of gameplay. Such as a player browser character skin options, etc.
-        /// </summary>
-        Browse,
+        [MonoPInvokeCallback(typeof(NoShowDelegate))]
+        private static void NoShowCallback()
+        {
+            // Called when a timeout was reached and an ad was not returned. This can occur due to ad blockers
+            // or other browser issues.
+            Debug.Log("[Wortal] NoShowCallback");
+        }
     }
 }
