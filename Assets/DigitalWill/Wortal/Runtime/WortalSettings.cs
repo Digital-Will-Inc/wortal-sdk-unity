3367ee6507ce28d47cd44aa57606df0b0a23a383
Tim Dillon <tim@itsdits.com>
2022-06-25 08:00:33 +0900

    feat: add wortal settings

---
+++ Assets/DigitalWill/Wortal/Runtime/WortalSettings.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace DigitalWill.H5Portal
+{
+    /// <summary>
+    /// Settings for the wortal.
+    /// </summary>
+    public class WortalSettings : ScriptableObject
+    {
+        [SerializeField] private string _linkInterstitialId;
+        [SerializeField] private string _linkRewardedId;
+
+        public string LinkInterstitialId => _linkInterstitialId;
+        public string LinkRewardedId => _linkRewardedId;
+    }
+}
