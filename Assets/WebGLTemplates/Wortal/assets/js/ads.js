5981b5f9e15d58db087dfbd1f5913c2b00cefbdd
Tim Dillon <tim@itsdits.com>
2022-06-24 12:00:11 +0900

    feat: add link wortal functions

--- Assets/WebGLTemplates/Wortal/assets/js/ads.js
+++ Assets/WebGLTemplates/Wortal/assets/js/ads.js
@@ -44,6 +44,40 @@ function showRewardedAdAdSense(name) {
   });
 }
 
+function showInterstitialAdLink(type, name) {
+  window.triggerWortalLinkAd(type, name, {
+    beforeAd: () => {
+      gameInstance.Module.asmLibraryArg._TriggerBeforeAdLink();
+    },
+    afterAd: () => {
+      gameInstance.Module.asmLibraryArg._TriggerAfterAdLink();
+    },
+    noShow: () => {
+      gameInstance.Module.asmLibraryArg._TriggerNoShowLink();
+    },
+  });
+}
+
+function showRewardedAdLink(name) {
+  window.triggerWortalLinkAd('reward', name, {
+    beforeAd: () => {
+      gameInstance.Module.asmLibraryArg._TriggerBeforeAdLink();
+    },
+    afterAd: () => {
+      gameInstance.Module.asmLibraryArg._TriggerAfterAdLink();
+    },
+    adDismissed: () => {
+      gameInstance.Module.asmLibraryArg._TriggerAdDismissedLink();
+    },
+    adViewed: () => {
+      gameInstance.Module.asmLibraryArg._TriggerAdViewedLink();
+    },
+    noShow: () => {
+      gameInstance.Module.asmLibraryArg._TriggerNoShowLink();
+    },
+  });
+}
+
 function triggerShowRewardedAdFn() {
   console.log("***** triggerShowRewardedAdFn *****");
   showRewardedAdFn();
