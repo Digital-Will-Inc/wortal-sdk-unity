9e76b23fd2b1ac30908576dfcb3178bf07d26aa4
Tim Dillon <tim@itsdits.com>
2022-07-07 13:26:05 +0900

    refactor: installer now only sets project settings

--- Assets/DigitalWill/Wortal/Editor/WortalSettingsEditor.cs
+++ Assets/DigitalWill/Wortal/Editor/WortalSettingsEditor.cs
@@ -4,12 +4,12 @@ using UnityEditor;
 namespace DigitalWill.Wortal.Editor
 {
     /// <summary>
-    ///
+    /// Editor for Wortal settings.
     /// </summary>
     [CustomEditor(typeof(WortalSettings))]
     public class WortalSettingsEditor : UnityEditor.Editor
     {
-        [MenuItem("DigitalWill/Wortal Settings")]
+        [MenuItem("DigitalWill/Wortal/Wortal Config")]
         private static void ShowSettingsMenu()
         {
             Selection.activeObject = H5Portal.Wortal.Settings;
