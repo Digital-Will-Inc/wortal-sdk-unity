229a79d5c6ed089746e75af85c2536a17adbc888
Tim Dillon <tim@itsdits.com>
2022-07-07 13:01:30 +0900

    feat: add localization settings

--- Assets/DigitalWill/Wortal/Runtime/WortalSettings.cs
+++ Assets/DigitalWill/Wortal/Runtime/WortalSettings.cs
@@ -11,7 +11,13 @@ namespace DigitalWill.H5Portal
         [SerializeField] private string _linkInterstitialId;
         [SerializeField] private string _linkRewardedId;
 
+        [Header("Localization")]
+        [SerializeField] private Language _defaultLanguage = Language.English;
+        [SerializeField] private Language[] _supportedLanguages = { Language.English, Language.Japanese };
+
         public string LinkInterstitialId => _linkInterstitialId;
         public string LinkRewardedId => _linkRewardedId;
+        public Language DefaultLanguage => _defaultLanguage;
+        public Language[] SupportedLanguages => _supportedLanguages;
     }
 }
