8d68b7480919e02839466817f8e2110e589ea452
Tim Dillon <tim@itsdits.com>
2022-11-25 15:40:37 +0900

    docs: add example for LocalizableContent

--- Assets/DigitalWill/Wortal/Runtime/WortalContext.cs
+++ Assets/DigitalWill/Wortal/Runtime/WortalContext.cs
@@ -317,6 +317,16 @@ namespace DigitalWill.WortalSDK
         /// <summary>
         /// Used to pass localized key-value pairs for content being used in the context API.
         /// </summary>
+        /// <remarks>If no localizable content is required then pass only the Default string.</remarks>
+        /// <example>
+        /// <code>var content = new LocalizableContent
+        ///     Default = "Play",
+        ///     Localizations = new Dictionary&lt;string, string&gt;
+        ///     {
+        ///         {"en_US", "Play"},
+        ///         {"ja_JP", "プレイ"},
+        ///     }</code>
+        /// </example>
         [Serializable]
         public struct LocalizableContent
         {
