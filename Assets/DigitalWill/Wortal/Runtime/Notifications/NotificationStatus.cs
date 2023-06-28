8f287f384f63b50455215ebe74060f689fda5c3c
Tim Dillon <tim@itsdits.com>
2023-06-28 15:24:58 +0900

    feat: add notifications api (#142)

---
+++ Assets/DigitalWill/Wortal/Runtime/Notifications/NotificationStatus.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace DigitalWill.WortalSDK
+{
+    /// <summary>
+    /// Status types of a scheduled notification.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum NotificationStatus
+    {
+        SCHEDULED,
+        SENT,
+        CANCELED,
+        UNKNOWN,
+    }
+}
